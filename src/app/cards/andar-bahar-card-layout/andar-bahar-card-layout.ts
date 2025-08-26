e473d3377d05b2790ff7c2a60689368bad0567f8
Dheeraj Yadav <yadavdheeraj417@gmail.com>
2025-08-27 02:55:25 +0530

    andar bahar card layout

--- src/app/cards/andar-bahar-card-layout/andar-bahar-card-layout.ts
+++ src/app/cards/andar-bahar-card-layout/andar-bahar-card-layout.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IData } from "../../models/casino.model";
 
 @Component({
@@ -7,6 +7,30 @@ import { IData } from "../../models/casino.model";
   templateUrl: './andar-bahar-card-layout.html',
   styleUrl: './andar-bahar-card-layout.css'
 })
-export class AndarBaharCardLayout {
+export class AndarBaharCardLayout implements OnInit {
 	@Input() gameData!: IData | null;
+	slideStore: Array<string> = [];
+	slideConfig = {
+		infinite: true,
+		speed: 4000,
+		slidesToShow: 7,
+		slidesToScroll: 3,
+		autoplay: true,
+		autoplaySpeed: 0,
+		cssEase: 'linear',
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		variableWidth: true
+	};
+
+	ngOnInit(): void {
+		if (this.gameData?.card) {
+			this.slideStore = this.gameData?.card.split(',').map(item => 'http://localhost:4200/v162/static/front/img/cards/' + item.trim() + '.png');
+			console.log(this.slideStore);
+		}
+	}
+
+	slickInit(): void { }
 }
